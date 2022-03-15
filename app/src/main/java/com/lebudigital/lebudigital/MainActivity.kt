cb94c4b5c11d40e59f2050262d44e8247912579c
Mukhammad Alfan <190710120@students.uajy.ac.id>
2022-03-15 09:00:22 +0700

    first project

---
+++ app/src/main/java/com/lebudigital/lebudigital/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lebudigital.lebudigital
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
